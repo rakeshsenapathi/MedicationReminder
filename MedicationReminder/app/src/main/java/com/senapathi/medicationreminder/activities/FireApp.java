cf168c371d710be440f55ca63a9c1a70d90c54b4
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-27 18:23:46 +0530

    Added FireApp class

---
+++ MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/activities/FireApp.java
@@ -0,0 +1,18 @@
+package com.senapathi.medicationreminder.activities;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Senapathi on 27-12-2016.
+ */
+
+public class FireApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+    }
+}
