48c77d88ef0c45679ceb1dd232758002b159a18c
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-27 17:07:42 +0530

    Updated Splash Screen

--- MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/activities/SplashScreenActivity.java
+++ MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/activities/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import com.senapathi.medicationreminder.R;
 
@@ -20,6 +21,7 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(new Runnable() {
             @Override
