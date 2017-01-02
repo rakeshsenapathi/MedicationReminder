60f0373439bc9000f2869e2ddd1ab65b2eb10fb0
rakeshsenapathi <rakeshsenapathi@gmail.com>
2017-01-03 00:11:41 +0530

    Implemented ResetPassword,SignUp functionalities

---
+++ MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/main/activities/ForgotPasswordActivity.java
@@ -0,0 +1,71 @@
+package com.senapathi.medicationreminder.main.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.senapathi.medicationreminder.R;
+import com.senapathi.medicationreminder.main.utils.Dialog;
+
+/**
+ * Created by Senapathi on 02-01-2017.
+ */
+
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmailField;
+    private Button mResetBtn;
+    private FirebaseAuth mAuth;
+    // Creating dialog object to access its dialog methods
+    Dialog dialog = new Dialog();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.forgotpassword);
+
+        //
+        mEmailField = (EditText) findViewById(R.id.reset_email);
+        mResetBtn = (Button) findViewById(R.id.resetbtn);
+        //
+
+        mResetBtn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email = mEmailField.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            dialog.showProgressDialog(ForgotPasswordActivity.this);
+            mAuth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ForgotPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(ForgotPasswordActivity.this, "Failed to reset password", Toast.LENGTH_SHORT).show();
+                            }
+                            dialog.closeProgressDialog(ForgotPasswordActivity.this);
+                        }
+                    });
+        }
+
+    }
+}
--- MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/main/activities/SignUpActivity.java
+++ MedicationReminder/app/src/main/java/com/senapathi/medicationreminder/main/activities/SignUpActivity.java
@@ -1,9 +1,10 @@
 package com.senapathi.medicationreminder.main.activities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -18,31 +19,33 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.senapathi.medicationreminder.R;
 import com.senapathi.medicationreminder.main.utils.Dialog;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+/**
+ * Created by Senapathi on 02-01-2017.
+ */
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mEmailField;
     private EditText mPwfField;
-    private Button loginButton;
-    private TextView forgotPwd;
-    private TextView mNewAccount;
+    private Button signupButton;
+    private TextView mLogin;
     private FirebaseAuth mAuth;
     // Creating dialog object to access its dialog methods
     Dialog dialog = new Dialog();
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Instantiating the FirebaseAuth Object
         mAuth = FirebaseAuth.getInstance();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signup);
 
-        //
-        mEmailField = (EditText) findViewById(R.id.emailedt);
-        mPwfField = (EditText) findViewById(R.id.pwdedt);
-        loginButton = (Button) findViewById(R.id.loginbtn);
-        forgotPwd = (TextView) findViewById(R.id.fpwdtext);
-        mNewAccount = (TextView) findViewById(R.id.newacctext);
+        // Typecasting finding the views
+        mEmailField = (EditText) findViewById(R.id.emailSignup);
+        mPwfField = (EditText) findViewById(R.id.pwdSignup);
+        signupButton = (Button) findViewById(R.id.signupbtn);
+        mLogin = (TextView) findViewById(R.id.logintext);
         //
 
         // AuthListener to get the current state of the user
@@ -50,74 +53,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser() != null) {
-                    dialog.closeProgressDialog(MainActivity.this);
-                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                    dialog.closeProgressDialog(SignUpActivity.this);
+                    Toast.makeText(SignUpActivity.this, "SignUp sucessful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignUpActivity.this,MainActivity.class));
                 }
             }
         };
         //
 
-        // on click listeners
-        loginButton.setOnClickListener(this);
-        forgotPwd.setOnClickListener(this);
-        mNewAccount.setOnClickListener(this);
+        // OnClick listeners
+        signupButton.setOnClickListener(this);
+        mLogin.setOnClickListener(this);
         //
-    }
 
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //Setting the Auth Listener to listen to the AuthStates
         mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
-    public void onStop() {
+    protected void onStop() {
         super.onStop();
-        //UnAuthorizing the user
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
 
-            // Click Listener for Login Button
-            case R.id.loginbtn:
+            //Click Listener for Login Button
+            case R.id.signupbtn:
                 String email = mEmailField.getText().toString().trim();
                 String password = mPwfField.getText().toString().trim();
-
                 // Checking for EmptyFields
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                    Toast.makeText(MainActivity.this, "Empty fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, "Empty fields", Toast.LENGTH_SHORT).show();
                 }
-                // If not found then SignIn with email,password and change the AuthState(CurrentUser!= null)
+                // If not found then SignUp with email,password to create a new user
                 else {
-                    dialog.showProgressDialog(MainActivity.this);
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    dialog.showProgressDialog(SignUpActivity.this);
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
                 }
                 break;
 
-            //Click Listener for ForgotPassword Button
-            case R.id.fpwdtext:
-                startActivity(new Intent(MainActivity.this,ForgotPasswordActivity.class));
-                break;
-
-            //Click Listener for Creating a NewAccount
-            case R.id.newacctext:
-                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
-                break;
-
+            // Click Listener for Login
+            case R.id.logintext:
+                Toast.makeText(SignUpActivity.this, "Login", Toast.LENGTH_SHORT).show();
         }
     }
 
